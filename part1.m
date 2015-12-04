72bab9c7e01dad5e6e62d115c5a847ae7be87d53
mborden <mborden@uccs.edu>
2015-12-04 14:52:12 -0700

    Com mit bitch

--- part1.m
+++ part1.m
@@ -81,46 +81,6 @@ T9 = stdface * inv(A9);
 A10 = [47, -2, 92; -141, -82, -83;1,1,1];
 T10 = stdface * inv(A10);
 
-%%A2 = [30, -30, 58; -91,-35,-23;1,1,1];
-%%T2 = stdface * inv(A2);
-
-%%transformation 3
-%%A3 = [87, 22, 99; -85, -31, -21;1,1,1];
-%%T3 = stdface * inv(A3);
-
-%%transformation 4
-%%A4 = [11, -41, 47; -38, 6, 13;1,1,1];
-%%T4 = stdface * inv(A4);
-
-%%transformation 5
-%%A5 = [51, 1, 88; 51, 1, -44;1,1,1];
-%%T5 = stdface * inv(A5);
-
-%%transformation 6
-%%A6 = [50, -1, 96; -62, -7, -4;1,1,1];
-%%T6 = stdface * inv(A6);
-
-%%transformation 7
-%%A7 = [60, -1, 95; -77, -1, -18;1,1,1];
-%%T7 = stdface * inv(A7);
-
-%%transformation 8
-%%A8 = [168, 100, 182; -71, -14, -13;1,1,1];
-%%T8 = stdface * inv(A8);
-
-%%transformation 9
-%%A9 = [6, -42, 54; 29, -42, 69;1,1,1];
-%%T9 = stdface * inv(A9);
-
-%%transformation 10
-%%A10 = [47, -2, 92; -141, -82, -83;1,1,1];
-%%T10 = stdface * inv(A10);
-
-
-%%coordinate transformation: maps input triangle to standard triangle
-%%x' = x *cos(theta) + y*cos(theta);
-%%y' = -x *sin(theta) + y*cos(theta);
-%%z' = z;
 
 %%warp geometric transformation 't' to images 1-10
 M = 10;
--- part2.m
+++ part2.m
@@ -3,7 +3,9 @@ clear all
 clf
 
 %%standard face triangle
-x = [50; -75;1];
-y = [1; -20;1];
-z = [75; -20;1];
+x = [50; -75];
+y = [1; -20];
+z = [75; -20];
 stdface = [x, y, z];
+
+%%normalizing images 
