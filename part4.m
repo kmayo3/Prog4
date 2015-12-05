6abba72bb93d7b87ff641170e28d5eb030ad44b6
kmayo3 <katlyn.mayo@ymail.com>
2015-12-04 21:43:31 -0700

    Fixed documentation

--- part4.m
+++ part4.m
@@ -1,15 +1,21 @@
+%%clear everything before it is run
 clc
 clear all
 clf
+
+%%image package
 pkg load image; 
+
 %%read in two images to be blended
-A = imread('C:\Users\Maegen\Documents\cs 1300\Prog4\small_muct.jpg/i000qa-fn.jpg');
-B = imread('C:\Users\Maegen\Documents\cs 1300\Prog4\small_muct.jpg/i000qb-fn.jpg');
+A = imread('C:\Users\Katlyn\Documents\GitHub\Prog4\small_muct.jpg/i000qa-fn.jpg');
+B = imread('C:\Users\Katlyn\Documents\GitHub\Prog4\small_muct.jpg/i000qb-fn.jpg');
 
 %%create a blended
 %%referenced from http://www.mathworks.com/help/images/ref/imfuse.html
-
 C = imfuse(A,B,'blend', 'Scaling', 'joint');
 
+%%write the matrix into the file
 imwrite(C, 'meshed_image.jpg');
+
+%%display the image
 imshow(C);
